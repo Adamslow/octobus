9ee582ebbc552e7679c970b28f5014bb20294e0d
Adamslow <46136212+Adamslow@users.noreply.github.com>
2019-01-28 13:37:38 +0000

    Add files via upload

---
+++ votebuttons.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Votebutton extends React.Component {
+
+    render() {
+      return(
+        <div>
+        <span>
+        <Votebutton />
+        <Votebutton />
+        </span>
+        <div/>
+      );
+    }
+
+
+
+}
+
+
+export default Votebutton
