aadf052be3368d345aac861421836460fbb84533
Adamslow <46136212+Adamslow@users.noreply.github.com>
2019-01-31 14:12:56 +0000

    Update buttons.jsx

--- buttons.jsx
+++ buttons.jsx
@@ -19,3 +19,56 @@ function Buttons() {
 }
 
 export default Buttons
+
+
+
+
+
+
+import React, {Component} from 'react';
+
+class Buttons extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: null
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+          handleClick() {
+            this.setState (prevState => {
+              return {
+                count: prevState.count +1
+              }
+            })
+
+  }
+
+    render() {
+      return (
+        <div>
+        <h1>{this.state.count}</h1>
+        <button
+        type="button"
+        className="btn btn-secondary"
+        onClick = {this.handleClick}>DEAL</button>
+        <button
+        onClick = {this.handleClick}
+        type="button"
+        className="btn btn-secondary">NO DEAL</button>
+        <button
+        onClick = {this.handleClick}
+        type="button"
+        className="btn btn-secondary">NO BREXIT</button>
+        </div>
+
+      );
+    }
+
+
+
+
+}
+
+export default Buttons
