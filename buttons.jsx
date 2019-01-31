b399d57de50c9ca709037fbbd0cabc4865dfa884
Adamslow <46136212+Adamslow@users.noreply.github.com>
2019-01-31 12:47:18 +0000

    Update buttons.jsx

--- buttons.jsx
+++ buttons.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 function Buttons() {
   return (
-    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-      <div class="btn-group mr-2" role="group" aria-label="First group">
-        <button type="button" class="btn btn-secondary">DEAL</button>
-
-      </div>
-      <div class="btn-group mr-2" role="group" aria-label="Second group">
-        <button type="button" class="btn btn-secondary">NO DEAL</button>
-
-      </div>
-      <div class="btn-group" role="group" aria-label="Third group">
-        <button type="button" class="btn btn-secondary">NO BREXIT</button>
-      </div>
+    <div>
+        <button 
+          type="button" 
+          class="btn btn-secondary">DEAL</button> 
+        <button 
+          type="button" 
+          class="btn btn-secondary">NO DEAL</button>
+        <button 
+          type="button" 
+          class="btn btn-secondary">NO BREXIT</button>
+      
     </div>
   );
 
