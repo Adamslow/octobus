772cb5937308af1641ff2b8597284f515d60c25a
Adamslow <46136212+Adamslow@users.noreply.github.com>
2019-02-04 23:11:34 +0000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -46,3 +46,60 @@ class App extends Component {
 
 
 export default App
+
+
+
+
+//Option with styles
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    console.log('constructor')
+    super(props)
+    this.state = {
+      voteDeal: null,
+      voteNodeal: null,
+      voteNobrexit: null,
+
+    };
+    this.addVoteDeal = this.addVoteDeal.bind(this)
+    this.addVoteNodeal = this.addVoteNodeal.bind(this)
+    this.addVoteNobrexit = this.addVoteNobrexit.bind(this)
+  }
+  addVoteDeal() {
+    this.setState({voteDeal: this.state.voteDeal + 1})
+  }
+  addVoteNodeal() {
+    this.setState ({ voteNodeal: this.state.voteNodeal +1})
+  }
+  addVoteNobrexit() {
+    this.setState ({ voteNobrexit: this.state.voteNobrexit +1})
+  }
+  render() {
+    return(
+      <body  style = {{textAlign: 'center',
+       background: 'linear-gradient(to bottom, #ff0066 0%, #660033 100%)'}}>
+      <div>
+      <h1>VOTE!!!</h1>
+      <h2>BREXIT</h2>
+      <h3>{this.state.voteDeal}</h3>
+      <button onClick = {this.addVoteDeal}>DEAL</button><br />
+      <h3>{this.state.voteNodeal}</h3>
+      <button onClick = {this.addVoteNodeal}>NO DEAL</button><br />
+      <h3>{this.state.voteNobrexit}</h3>
+      <button onClick = {this.addVoteNobrexit}>NO BREXIT</button>
+
+
+      </div>
+      </body>
+    );
+  }
+};
+
+
+
+
+
+
+export default App
